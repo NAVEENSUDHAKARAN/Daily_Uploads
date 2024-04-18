9287325c962ab91f0e0ce6519ed0d95e437224f5
Nave3557 <Nave3557@CSL0700.global.chainsys.com>
2024-04-18 17:28:31 +0530

    Anagrams

--- Programming_Challenges/src/com/chainsys/programmingchallenges/Anagrams.java
+++ Programming_Challenges/src/com/chainsys/programmingchallenges/Anagrams.java
@@ -1,9 +1,33 @@
 package com.chainsys.programmingchallenges;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Anagrams {
 
+	public static boolean isAnagram(String str1, String str2) {
+		
+		char[] ch1 = str1.toCharArray();
+		char[] ch2 = str2.toCharArray();
+		
+		Arrays.sort(ch1);
+		Arrays.sort(ch2);
+		
+		if(Arrays.equals(ch1, ch2))
+		{
+			return true;
+		}
+		return false;
+	}
+	
 	public static void main(String[] args)
 	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter The First Word : ");
+		String str1 = scanner.next();
+		System.out.println("Enter The Second Word : ");
+		String str2 = scanner.next();
 		
+		System.out.println(isAnagram(str1, str2));
 	}
 }
